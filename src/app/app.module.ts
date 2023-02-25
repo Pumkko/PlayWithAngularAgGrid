8d9006826e0a07dec5c9340c94f14e8b8b0ca484
ccoque <c.coque@hotmail.fr>
2023-02-25 10:55:49 -0500

    learning single page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'crisis-list', component: CrisisListComponent },
+      { path: 'heroes-list', component: HeroesListComponent },
+      { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
+      { path: '**', component: NotFoundComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
