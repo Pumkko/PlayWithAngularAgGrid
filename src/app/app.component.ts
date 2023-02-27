859669f6879228e46a3e67a31dba5a622d028fc2
ccoque <c.coque@hotmail.fr>
2023-02-26 21:59:43 -0500

    add healthcheck and sync logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChangeService } from './change.service';
+import { NetworkService } from './network.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tryStuff';
+
+
+  changeWaitingToBeSync = 0;
+
+  get isOnline$() { 
+    return this.networkService.healthCheck$;
+  }
+  
+
+  constructor(private changeService: ChangeService, private networkService: NetworkService) {
+    this.changeService.changeToSync$.subscribe(v => this.changeWaitingToBeSync = v.length)
+  }
+
+  async onSynchronize() {
+    await this.changeService.synchronizeChange();
+  }
 }
--- src/app/network.service.ts
+++ src/app/network.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { fromFetch } from 'rxjs/fetch';
-import { catchError, interval, of, switchMap } from 'rxjs';
+import { catchError, interval, map, of, share, shareReplay, switchMap, tap, timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
 
-  healthCheck$ = interval(30 * 1000)
-    .pipe( switchMap(() => fromFetch('https://api.github.com')))
-    .pipe (
-      switchMap(() => of(true)),
-      catchError(() => of(false))
-    )
 
+  healthCheck$ = timer(0, 30 * 1000)
+    .pipe( 
+      switchMap(() => 
+        fromFetch('https://api.github.com')
+          .pipe(
+            catchError(() => of(false)),
+            map(v => v !== false)
+          )
+      ),
+      shareReplay(1))
+    
 }
