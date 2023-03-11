938e098002a2666390c2fa9cceb976a35a0d6764
ccoque <c.coque@hotmail.fr>
2023-03-11 11:50:38 -0500

    set rickandmorty routing in rickandmorty module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { RouterModule } from '@angular/router';
 import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CharacterComponent } from './rick-and-morty/character/character.component';
-import { EpisodeComponent } from './rick-and-morty/episode/episode.component';
 import { RickAndMortyModule } from './rick-and-morty/rick-and-morty.module';
 
 @NgModule({
@@ -14,8 +12,6 @@ import { RickAndMortyModule } from './rick-and-morty/rick-and-morty.module';
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: 'rick-and-morty-characters', component: CharacterComponent },
-      { path: 'rick-and-morty-episodes', component: EpisodeComponent },
       { path: '', redirectTo: '/rick-and-morty-characters', pathMatch: 'full' },
       { path: '**', component: NotFoundComponent },
     ]),
--- src/app/rick-and-morty/rick-and-morty.module.ts
+++ src/app/rick-and-morty/rick-and-morty.module.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AgGridModule } from 'ag-grid-angular';
 import { CharacterComponent } from './character/character.component';
+import { EpisodeComponent } from './episode/episode.component';
 import { SpecieHeaderComponent } from './specie-header/specie-header.component';
 import { TotalAlienService } from './total-alien.service';
-import { EpisodeComponent } from './episode/episode.component';
 
 @NgModule({
   declarations: [CharacterComponent, SpecieHeaderComponent, EpisodeComponent],
   providers: [TotalAlienService],
-  imports: [CommonModule, AgGridModule],
+  imports: [
+    CommonModule,
+    AgGridModule,
+    RouterModule.forChild([
+      { path: 'rick-and-morty-characters', component: CharacterComponent },
+      { path: 'rick-and-morty-episodes', component: EpisodeComponent },
+    ]),
+  ],
 })
 export class RickAndMortyModule {}
