8bc850e292d4dee1951126c9354a36d2d99cb9ba
ccoque <c.coque@hotmail.fr>
2023-03-11 11:49:21 -0500

    add episode component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { AgGridModule } from 'ag-grid-angular';
+import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { CharacterComponent } from './rick-and-morty/character/character.component';
+import { EpisodeComponent } from './rick-and-morty/episode/episode.component';
 import { RickAndMortyModule } from './rick-and-morty/rick-and-morty.module';
-import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotFoundComponent,
-  ],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: 'rick-and-morty', component: CharacterComponent },
-      { path: '', redirectTo: '/rick-and-morty', pathMatch: 'full' },
+      { path: 'rick-and-morty-characters', component: CharacterComponent },
+      { path: 'rick-and-morty-episodes', component: EpisodeComponent },
+      { path: '', redirectTo: '/rick-and-morty-characters', pathMatch: 'full' },
       { path: '**', component: NotFoundComponent },
     ]),
     AgGridModule,
-    RickAndMortyModule
+    RickAndMortyModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/rick-and-morty/rick-and-morty.module.ts
+++ src/app/rick-and-morty/rick-and-morty.module.ts
@@ -4,9 +4,10 @@ import { AgGridModule } from 'ag-grid-angular';
 import { CharacterComponent } from './character/character.component';
 import { SpecieHeaderComponent } from './specie-header/specie-header.component';
 import { TotalAlienService } from './total-alien.service';
+import { EpisodeComponent } from './episode/episode.component';
 
 @NgModule({
-  declarations: [CharacterComponent, SpecieHeaderComponent],
+  declarations: [CharacterComponent, SpecieHeaderComponent, EpisodeComponent],
   providers: [TotalAlienService],
   imports: [CommonModule, AgGridModule],
 })
