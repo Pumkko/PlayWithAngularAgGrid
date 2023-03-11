c0554b515476d9719ecd0545093bb03490a5a65c
ccoque <c.coque@hotmail.fr>
2023-03-11 11:56:11 -0500

    forgot two files

--- src/app/database/db.ts
+++ src/app/database/db.ts
@@ -1,21 +1,18 @@
-import Dexie, { Table } from "dexie";
-import { RickAndMortyCharacter } from "./rickAndMortyCharacter";
-import { RickAndMortyEpisode } from "./rickAndMortyEpisode";
+import Dexie, { Table } from 'dexie';
+import { RickAndMortyCharacter } from './rickAndMortyCharacter';
+import { RickAndMortyEpisode } from './rickAndMortyEpisode';
 
 export class AppDb extends Dexie {
+  rickAndMortyCharacters!: Table<RickAndMortyCharacter, number>;
+  rickAndMortyEpisodes!: Table<RickAndMortyEpisode, number>;
 
-    rickAndMortyCharacters!: Table<RickAndMortyCharacter, number>;
-    rickAndMortyEpisodes!: Table<RickAndMortyEpisode, number>;
-
-    constructor() {
-        super('ngdexieliveQuery');
-        this.version(4).stores({
-            rickAndMortyCharacters: '++id',
-            rickAndMortyEpisodes: '++id'
-        });
-
-      }
-
+  constructor() {
+    super('ngdexieliveQuery');
+    this.version(4).stores({
+      rickAndMortyCharacters: '++id',
+      rickAndMortyEpisodes: '++id',
+    });
+  }
 }
 
 export const db = new AppDb();
